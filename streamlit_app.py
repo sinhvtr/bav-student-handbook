81f003a59b8bfcf5cc854688fd33035d4c6605e9
sinhvtr <34147736+sinhvtr@users.noreply.github.com>
2022-01-06 10:05:22 +0700

    Remove submit button

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,16 +16,16 @@ bm25 = BM25Okapi(tokenized_corpus)
 st.title("Hệ thống tra cứu quy chế sinh viên Học viện Ngân hàng")
 st.text("Dựa trên nội dung phần II cuốn Sổ tay sinh viên năm 2017")
 
-form = st.form(key='my_form')
+# form = st.form(key='my_form')
 text_input = form.text_input(label='Viết câu hỏi của bạn')
-submit_button = form.form_submit_button(label='Tra cứu')
-if submit_button:
-    tokenized_query = text_input.split(" ")
-    doc_scores = bm25.get_scores(tokenized_query).tolist()
-    doc_scores = sorted(doc_scores, reverse=True)
-    top_n = bm25.get_top_n(tokenized_query, corpus, n=3)
+# submit_button = form.form_submit_button(label='Tra cứu')
+# if submit_button:
+tokenized_query = text_input.split(" ")
+doc_scores = bm25.get_scores(tokenized_query).tolist()
+doc_scores = sorted(doc_scores, reverse=True)
+top_n = bm25.get_top_n(tokenized_query, corpus, n=3)
 
-    st.markdown('**{}**'.format(top_n[0]))
-    st.markdown('*Một số kết quả khác:*')
-    for result in top_n[1:]:
-        st.write(result)
+st.markdown('**{}**'.format(top_n[0]))
+st.markdown('*Một số kết quả khác:*')
+for result in top_n[1:]:
+    st.write(result)
